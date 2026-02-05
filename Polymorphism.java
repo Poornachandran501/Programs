34c3e8882f3c94bcce8f7e87efd0c2c62927b693
Poorna Chandran <cpoorna414@gmail.com>
2026-02-05 10:24:49 +0530

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,22 @@
+package Newprograms;
+
+public class Polymorphism {
+
+    public  static void main(String[] args) {
+    	System.out.println(num());
+    	System.out.println (multiply());
+    }
+    public static int num () {
+    	int a =13;
+    	return a;
+    }
+    public static int multiply() {;
+    int a=5;
+    int b=3;
+    return a*b;
+    }
+      
+}
+    	
+    
+     
