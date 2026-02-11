f6b5883cff7df640027672b366fe5994db4dd101
Poorna Chandran <cpoorna414@gmail.com>
2026-02-11 12:04:45 +0530

    Add files via upload

---
+++ Integer2.java
@@ -0,0 +1,14 @@
+package Coreproject;
+
+public class Integer2 {
+    public static void main(String[] args) {
+   int[] ascii = {80,111,111,114,110,97};    
+    	String str = "";
+
+     for (int i = 0; i < ascii.length; i++) {
+    str = str + (char) ascii[i];
+    			        }
+System.out.println(str);
+    			    }
+    			}
+
